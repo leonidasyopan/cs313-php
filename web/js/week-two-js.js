96051abc32ddd2cc15ba59de0ddea72b3e8cef91
Leonidas Yopan <leonidasyopan@gmail.com>
2020-01-14 21:59:57 -0300

    comments describing JS functions

--- web/js/week-two-js.js
+++ web/js/week-two-js.js
@@ -1,10 +1,13 @@
+/* Variables representing both buttons */
 var clickButton = window.document.querySelector('#click-me');
 var changeColorButton = window.document.querySelector('#change-color-button');
 
+/* Function to alert when button is clicked. */
 function clicked() {
     alert("Clicked!");    
 }
 
+/* Function to change color of div according to user's input*/
 function changeDivColor() {
     var fistDiv = window.document.querySelector('#first-div');
     var inputInfo = window.document.querySelector('#box-one-color').value;
@@ -13,5 +16,6 @@ function changeDivColor() {
 
 }
 
+/* Run functions when buttons are clicled */
 clickButton.addEventListener("click", clicked);
 changeColorButton.addEventListener("click", changeDivColor);
