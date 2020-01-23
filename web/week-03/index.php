3cd6ff56ac1c00fee0f74f77616dd740798d2411
Leonidas Yopan <leonidasyopan@gmail.com>
2020-01-23 08:35:11 -0300

    completed styling for the items of ecommerce

--- web/week-03/index.php
+++ web/week-03/index.php
@@ -24,86 +24,97 @@
             <section id="instruments-list">
                 <div class="instrument-offer">
                     <figure>
-                        <img src="img/acoustic-guitar-01.jpg" alt="Acoustic Guitar Beginners">
+                        <img src="img/acoustic-guitar-01.jpg" alt="Acoustic Guitar Beginners" class="instrument-image">
                         <figcaption>Acoustic Guitar Beginners</figcaption>
                     </figure>
 
                     <div class="price">
-                        <p></p>
+                        <p><span class="price-symbol">$</span> 115.50</p>
                     </div>
-
-                    <a href="#" class="more-details">More Details</a>
-                    <a href="#" class="add-to-cart"><svg data-name="Layer 2" height="100" id="Layer_2" viewBox="0 0 100 100" width="100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    <div class="details-cart">
+                        <a href="#" class="more-details">More Details</a>
+                        <a href="#" class="add-to-cart"><svg data-name="Layer 2" id="Layer_2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    </div>
                 </div>
 
                 <div class="instrument-offer">
                     <figure>
-                        <img src="img/acoustic-guitar-02.jpg" alt="Acoustic Guitar Premium">
+                        <img src="img/acoustic-guitar-02.jpg" alt="Acoustic Guitar Premium" class="instrument-image">
                         <figcaption>Acoustic Guitar Premium</figcaption>
                     </figure>
 
                     <div class="price">
-                        <p></p>
+                        <p><span class="price-symbol">$</span> 139.50</p>
                     </div>
 
-                    <a href="#" class="more-details">More Details</a>
-                    <a href="#" class="add-to-cart"><svg data-name="Layer 2" height="100" id="Layer_2" viewBox="0 0 100 100" width="100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    <div class="details-cart">
+                        <a href="#" class="more-details">More Details</a>
+                        <a href="#" class="add-to-cart"><svg data-name="Layer 2" id="Layer_2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    </div>
                 </div>
 
                 <div class="instrument-offer">
                     <figure>
-                        <img src="img/guitar-01.jpg" alt="Classic Electric Guitar">
+                        <img src="img/guitar-01.jpg" alt="Classic Electric Guitar" class="instrument-image">
                         <figcaption>Classic Electric Guitar</figcaption>
                     </figure>
 
                     <div class="price">
-                        <p></p>
+                        <p><span class="price-symbol">$</span> 259.50</p>
                     </div>
 
-                    <a href="#" class="more-details">More Details</a>
-                    <a href="#" class="add-to-cart"><svg data-name="Layer 2" height="100" id="Layer_2" viewBox="0 0 100 100" width="100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    <div class="details-cart">
+                        <a href="#" class="more-details">More Details</a>
+                        <a href="#" class="add-to-cart"><svg data-name="Layer 2" id="Layer_2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    </div>
                 </div>
 
                 <div class="instrument-offer">
                     <figure>
-                        <img src="img/guitar-03.jpg" alt="Stylist Electric Guitar">
+                        <img src="img/guitar-03.jpg" alt="Stylist Electric Guitar" class="instrument-image">
                         <figcaption>Stylist Electric Guitar</figcaption>
                     </figure>
 
                     <div class="price">
-                        <p></p>
+                        <p><span class="price-symbol">$</span> 189.50</p>
                     </div>
 
-                    <a href="#" class="more-details">More Details</a>
-                    <a href="#" class="add-to-cart"><svg data-name="Layer 2" height="100" id="Layer_2" viewBox="0 0 100 100" width="100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    <div class="details-cart">
+                        <a href="#" class="more-details">More Details</a>
+                        <a href="#" class="add-to-cart"><svg data-name="Layer 2" id="Layer_2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    </div>
                 </div>
 
                 <div class="instrument-offer">
                     <figure>
-                        <img src="img/ukulele-01.jpg" alt="Modern Ukulele">
+                        <img src="img/ukulele-01.jpg" alt="Modern Ukulele" class="instrument-image">
                         <figcaption>Modern Ukulele</figcaption>
                     </figure>
 
                     <div class="price">
-                        <p></p>
+                        <p><span class="price-symbol">$</span> 105.50</p>
                     </div>
 
-                    <a href="#" class="more-details">More Details</a>
-                    <a href="#" class="add-to-cart"><svg data-name="Layer 2" height="100" id="Layer_2" viewBox="0 0 100 100" width="100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    <div class="details-cart">
+                        <a href="#" class="more-details">More Details</a>
+                        <a href="#" class="add-to-cart"><svg data-name="Layer 2" id="Layer_2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    </div>
                 </div>
 
                 <div class="instrument-offer">
                     <figure>
-                        <img src="img/viola-01.jpg" alt="Amazing Viola">
+                        <img src="img/viola-01.jpg" alt="Amazing Viola" class="instrument-image">
                         <figcaption>Amazing Viola</figcaption>
                     </figure>
 
                     <div class="price">
-                        <p></p>
+                        <p><span class="price-symbol">$</span> 325.50</p>
                     </div>
 
-                    <a href="#" class="more-details">More Details</a>
-                    <a href="#" class="add-to-cart"><svg data-name="Layer 2" height="100" id="Layer_2" viewBox="0 0 100 100" width="100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    <div class="details-cart">
+                        <a href="#" class="more-details">More Details</a>
+                        <a href="#" class="add-to-cart"><svg data-name="Layer 2" id="Layer_2" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><title/><path d="M37,88a8,8,0,1,1,8-8A8,8,0,0,1,37,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,37,78Z"/><path d="M63,88a8,8,0,1,1,8-8A8,8,0,0,1,63,88Zm0-10a2,2,0,1,0,2,2A2,2,0,0,0,63,78Z"/><path d="M32,60H66a3.1,3.1,0,0,0,.54,0l.17,0,.35-.11.18-.07a2.43,2.43,0,0,0,.35-.2.33.33,0,0,0,.1-.06,1.8,1.8,0,0,0,.4-.33l.1-.1a1.76,1.76,0,0,0,.24-.3l.1-.15a3.14,3.14,0,0,0,.18-.31c0-.06.05-.11.07-.17l0-.07,13-35A3,3,0,1,0,76.19,21l-1.13,3H24.94l-1.13-3A3,3,0,0,0,21,19H14a3,3,0,0,0,0,6h4.91L29.8,54.31A8,8,0,0,0,32,70H73a3,3,0,0,0,0-6H32a2,2,0,0,1,0-4ZM44,39h3V36a3,3,0,0,1,6,0v3h3a3,3,0,0,1,0,6H53v3a3,3,0,0,1-6,0V45H44a3,3,0,0,1,0-6Z"/></svg></a>
+                    </div>
                 </div>            
             </section>
         </div>
---
+++ web/week-03/js/application.js
@@ -0,0 +1,17 @@
+// Function on execution of image zoom effect.
+/*
+var instrumentImage = document.querySelector('.instrument-image');
+
+function zoomIn() {
+    instrumentImage.style.transform = "scale(1.2)";
+    instrumentImage.style.transition = "all 1s";
+}
+
+function zoomOut() {
+    instrumentImage.style.transform = "scale(1)";
+    instrumentImage.style.transition = "all 1s";
+}
+
+instrumentImage.addEventListener("mouseover", zoomIn);
+instrumentImage.addEventListener("mouseleave", zoomOut);
+*/
