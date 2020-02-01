ba4c38ff87b05b82eb5a2d218bde7ab7efbee8aa
Leonidas Yopan <leonidasyopan@gmail.com>
2020-02-01 18:02:55 -0300

    tried to connect to database

---
+++ web/mession/index.php
@@ -0,0 +1,24 @@
+<?php require "connection/connect.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>meSSION 2</title>
+</head>
+<body>
+    
+    <?php
+
+        foreach ($db->query('SELECT username, password FROM users') as $row)
+        {
+            echo 'user: ' . $row['username'];
+            echo ' password: ' . $row['password'];
+            echo '<br/>';
+        }
+
+    ?>
+
+</body>
+</html>
